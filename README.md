421caec993e1468cc08ec2bd575f601d97e4f167
Emmett Pickerel <epickerel@realgravity.com>
2011-06-15 11:52:30 -0700

    First commit of code

---
+++ README.md
@@ -0,0 +1,13 @@
+tinyMS: tiny multi select
+=========================
+
+tinyMS is a super-lightweight (1.3k minified) multiselect enhancement jQuery widget. It preserves but hides the select element, presenting an "Add..." link which pops up the list of items. The user may then select or deselect as many items as they like, then click away from the list to close it. Selected items will be listed adjacent to the add link.
+
+I wrote it because I was frustrated that all the multiple select enhancement widgets I could find were 30k and up.
+
+
+To do:
+------
+* Add option for changing the add link text
+* Add basic css
+* Create a demo
---
+++ jquery.tinyms.js
@@ -0,0 +1,98 @@
+(function($){
+"use strict";
+//multi select replacement -- no optgroup support!
+var widgetname = 'ui-widget-multiselect',
+  classwidgetname = ' class="' + widgetname,
+  ui_state_active = 'ui-state-active',
+  selectListItemTemplate = '<li><a href="#">{l}</a></li>',
+  $body = $('body'),
+  create, ddMultilist, populateList;
+
+ddMultilist = function($ul, onChange, $offsetEl){
+  var toggle, opening, open, shut;
+  $ul.appendTo($body).css('position', 'absolute');
+  toggle = function($selectLi) {
+    var selected = !$selectLi.hasClass(ui_state_active);
+    $selectLi.toggleClass(ui_state_active)
+      .data('option').selected = selected;
+    onChange();
+  };
+  $ul.click(function(ev){
+    ev.preventDefault();
+    var t = ev.target;
+    toggle(t.nodeName === 'LI' ? $(t) : $(t).closest('li'));
+  });
+  $body.click(function(ev){
+    if (!opening) {
+      var t = $(ev.target);
+      if (t.closest('ul.' + widgetname + '-select').length === 0) {
+        $ul.removeClass(ui_state_active);
+      }
+    } else {
+      opening = false;
+    }
+  });
+  open = function(){
+    opening = true;
+    $ul.css($offsetEl.offset())
+      .addClass(ui_state_active);
+  };
+  shut = function(){
+    $ul.removeClass(ui_state_active);
+  };
+  return {
+    open: open,
+    shut: shut
+  };
+};
+
+populateList = function($select, $list){
+  var options = $select[0].options, arr = [], i, l;
+  for (i=0, l=options.length; i<l; i++) {
+    if (options[i].selected) {
+      arr.push($(options[i]).text());
+    }
+  }
+  $list.text(arr.join(', '));
+};
+
+create = function($select){
+  var $outer = $([
+      '<div', classwidgetname, '">',
+        '<a', classwidgetname, '-add"><span>Add...</span></a>',
+        '<ul', classwidgetname, '-select"></ul>',
+        '<p', classwidgetname, '-list"></p>',
+      '</div>'].join('')),
+    $selectUl = $outer.children('ul.' + widgetname + '-select:first'),
+    $list = $outer.children('.' + widgetname + '-list:first'),
+    $options = $select.children('option'),
+    $add = $outer.children('a.' + widgetname + '-add:first'),
+    ddml;
+  $options.each(function(){
+    $( selectListItemTemplate.replace('{l}', this.innerHTML) )
+        .data('option', this)
+        .appendTo($selectUl);
+  });
+  $outer.insertBefore($select);
+  $select.css({
+    position: 'absolute',
+    left: '-999em'
+  });
+  ddml = ddMultilist($selectUl, function(){
+    populateList($select, $list);
+  }, $add);
+  $add.click(function(ev){
+    ev.preventDefault();
+    ddml.open();
+  });
+  populateList($select, $list);
+};
+
+$.fn.tinyMS = function(){
+  this.each(function(){
+    create($(this));
+  });
+  return this;
+};
+
+}(window.jQuery));
